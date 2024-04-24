7c44ecd6d4bc53df2aa67e23cabb7129d20bd097
Alex <alexander.schlueter86@gmx.de>
2024-04-24 13:26:17 +0200

    surfing bc and postprocessing

--- shared/scripts/01-phasefield-fracture-porous/3D-surfing/pfmfrac.py
+++ shared/scripts/01-phasefield-fracture-porous/3D-surfing/pfmfrac.py
@@ -1,3 +1,4 @@
+import alex.linearelastic
 import dolfinx as dlfx
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -6,6 +7,7 @@ import ufl
 import numpy as np
 import os 
 import sys
+import math
 
 import alex.os
 import alex.phasefield as pf
@@ -18,14 +20,8 @@ script_name_without_extension = os.path.splitext(os.path.basename(__file__))[0]
 logfile_path = alex.os.logfile_full_path(script_path,script_name_without_extension)
 outputfile_xdmf_path = alex.os.outputfile_xdmf_full_path(script_path,script_name_without_extension)
 
-
-def mpi_print(output):
-    if rank == 0:
-        print(output)
-        sys.stdout.flush
-    return
 # set FEniCSX log level
-# dlfx.log.set_log_level(dlfx.log.LogLevel.INFO)
+# dlfx.log.set_log_level(log.LogLevel.INFO)
 # dlfx.log.set_output_file('xxx.log')
 
 # set and start stopwatch
@@ -39,78 +35,41 @@ size = comm.Get_size()
 print('MPI-STATUS: Process:', rank, 'of', size, 'processes.')
 sys.stdout.flush()
 
-with dlfx.io.XDMFFile(comm, os.path.join(alex.os.resources_directory,'foam_mesh.xdmf'), 'r') as mesh_inp: 
-    domain = mesh_inp.read_mesh()
+# mesh 
+N = 16 
 
-Tend = 0.01
-dt = 0.0001
+# generate domain
+#domain = dlfx.mesh.create_unit_square(comm, N, N, cell_type=dlfx.mesh.CellType.quadrilateral)
+domain = dlfx.mesh.create_unit_cube(comm,N,N,N,cell_type=dlfx.mesh.CellType.hexahedron)
 
-# # elastic constants
-# lam = dlfx.fem.Constant(domain, 10.0)
-# mu = dlfx.fem.Constant(domain, 10.0)
-
-# # residual stiffness
-# eta = dlfx.fem.Constant(domain, 0.001)
-
-# # phase field parameters
-# Gc = dlfx.fem.Constant(domain, 1.0)
-# epsilon = dlfx.fem.Constant(domain, 0.05)
-# Mob = dlfx.fem.Constant(domain, 1.0)
-# iMob = dlfx.fem.Constant(domain, 1.0/Mob.value)
-
-
-# function space using mesh and degree
-Ve = ufl.VectorElement("Lagrange", domain.ufl_cell(), 1) # displacements
-# V = dlfx.fem.FunctionSpace(domain,Ve)
-Te = ufl.FiniteElement("Lagrange", domain.ufl_cell(),2) # fracture fields
-W = dlfx.fem.FunctionSpace(domain, ufl.MixedElement([Ve, Te]))
-
-# get dimension and bounds for each mpi process
-dim = domain.topology.dim
-x_min = np.min(domain.geometry.x[:,0]) 
-x_max = np.max(domain.geometry.x[:,0])   
-y_min = np.min(domain.geometry.x[:,1]) 
-y_max = np.max(domain.geometry.x[:,1])   
-z_min = np.min(domain.geometry.x[:,2]) 
-z_max = np.max(domain.geometry.x[:,2])
-
-# find global min/max over all mpi processes
-comm.Barrier()
-x_min_all = comm.allreduce(x_min, op=MPI.MIN)
-x_max_all = comm.allreduce(x_max, op=MPI.MAX)
-y_min_all = comm.allreduce(y_min, op=MPI.MIN)
-y_max_all = comm.allreduce(y_max, op=MPI.MAX)
-z_min_all = comm.allreduce(z_min, op=MPI.MIN)
-z_max_all = comm.allreduce(z_max, op=MPI.MAX)
-comm.Barrier()
-
-mpi_print('spatial dimensions: '+str(dim))
-mpi_print('x_min, x_max: '+str(x_min_all)+', '+str(x_max_all))
-mpi_print('y_min, y_max: '+str(y_min_all)+', '+str(y_max_all))
-mpi_print('z_min, z_max: '+str(z_min_all)+', '+str(z_max_all))
+Tend = 0.4
+dt = 0.05
 
 # elastic constants
 lam = dlfx.fem.Constant(domain, 10.0)
 mu = dlfx.fem.Constant(domain, 10.0)
 
 # residual stiffness
-eta = dlfx.fem.Constant(domain, 0.01)
+eta = dlfx.fem.Constant(domain, 0.001)
 
 # phase field parameters
 Gc = dlfx.fem.Constant(domain, 1.0)
-# epsilon = dlfx.fem.Constant(domain, 0.3*(x_max_all - x_min_all))
-epsilon = dlfx.fem.Constant(domain, 100.0)
-Mob = dlfx.fem.Constant(domain, 1000.0)
+epsilon = dlfx.fem.Constant(domain, 0.05)
+Mob = dlfx.fem.Constant(domain, 1.0)
 iMob = dlfx.fem.Constant(domain, 1.0/Mob.value)
+
+
+# function space using mesh and degree
+Ve = ufl.VectorElement("Lagrange", domain.ufl_cell(), 1) # displacements
+Te = ufl.FiniteElement("Lagrange", domain.ufl_cell(), 2) # fracture fields
+W = dlfx.fem.FunctionSpace(domain, ufl.MixedElement([Ve, Te]))
 
 # define crack by boundary
 def crack(x):
-    x_log = x[0]< (0.05*(x_max_all-x_min_all) + x_min_all)
-    y_log = np.isclose(x[1],(y_max_all / 2.0),atol=(0.02*((y_max_all-y_min_all))))
-    return np.logical_and(y_log,x_log)
+    return np.logical_and(np.isclose(x[1], 0.5), x[0]<0.25) 
 
 eps_mac = dlfx.fem.Constant(domain, np.array([[0.0, 0.0, 0.0],
-                    [0.0, 0.1, 0.0],
+                    [0.0, 0.6, 0.0],
                     [0.0, 0.0, 0.0]]))
 
 # # define boundary condition on top and bottom
@@ -118,7 +77,16 @@ fdim = domain.topology.dim -1
 crackfacets = dlfx.mesh.locate_entities(domain, fdim, crack)
 crackdofs = dlfx.fem.locate_dofs_topological(W.sub(1), fdim, crackfacets)
 bccrack = dlfx.fem.dirichletbc(0.0, crackdofs, W.sub(1))
-bcs = bc.get_total_linear_displacement_boundary_condition_at_box(domain, comm, W,0,eps_mac)
+
+# def surfing_boundary(x):
+
+E_mod = alex.linearelastic.get_emod(lam.value, mu.value)
+K1 = dlfx.fem.Constant(domain, 1.5 * math.sqrt(Gc.value*E_mod))
+xtip = np.array([0.25, 0.5])
+xK1 = dlfx.fem.Constant(domain, xtip)
+
+bcs = bc.get_total_surfing_boundary_condition_at_box(domain,comm,W,0,K1,xK1,lam,mu,epsilon.value)
+# bcs = bc.get_total_linear_displacement_boundary_condition_at_box(domain, comm, W,0,eps_mac)
 bcs.append(bccrack)
 
 
@@ -145,11 +113,10 @@ def before_each_time_step(t,dt):
     if rank == 0:
         sol.print_time_and_dt(t,dt)
         
+phaseFieldProblem = pf.StaticPhaseFieldProblem3D(degradationFunction=pf.degrad_quadratic,
+                                                   psisurf=pf.psisurf)
 
 def get_residuum_and_gateaux(delta_t: dlfx.fem.Constant):
-    phaseFieldProblem = pf.StaticPhaseFieldProblem3D(degradationFunction=pf.degrad_quadratic,
-                                                   psisurf=pf.psisurf)
-    
     [Res, dResdw] = phaseFieldProblem.prep_newton(
         w=w,wm1=wm1,dw=dw,ddw=ddw,lam=lam, mu = mu,
         Gc=Gc,epsilon=epsilon, eta=eta,
@@ -157,15 +124,37 @@ def get_residuum_and_gateaux(delta_t: dlfx.fem.Constant):
     return [Res, dResdw]
     
 def get_bcs(t):
+    v_crack = 0.75/0.4
+    xtip = np.array([0.25 + v_crack * t, 0.5])
+    xK1 = dlfx.fem.Constant(domain, xtip)
+
+    bcs = bc.get_total_surfing_boundary_condition_at_box(domain,comm,W,0,K1,xK1,lam,mu,epsilon.value)
+    # bcs = bc.get_total_linear_displacement_boundary_condition_at_box(domain, comm, W,0,eps_mac)
+    bcs.append(bccrack)
     # can be updated here
     return bcs
 
+n = ufl.FacetNormal(domain)
 def after_timestep_success(t,dt,iters):
     pp.write_phasefield_mixed_solution(domain,outputfile_xdmf_path, w, t, comm)
 
     # write to newton-log-file
     if rank == 0:
         sol.write_to_newton_logfile(logfile_path,t,dt,iters)
+        
+    
+    # compute J-Integral
+    eshelby = phaseFieldProblem.getEshelby(w,eta,lam,mu)
+    J3D_loc_x, J3D_loc_y, J3D_loc_z = alex.linearelastic.get_J_3D(eshelby, n)
+    
+    comm.Barrier()
+    J3D_glob_x = comm.allreduce(J3D_loc_x, op=MPI.SUM)
+    J3D_glob_y = comm.allreduce(J3D_loc_y, op=MPI.SUM)
+    J3D_glob_z = comm.allreduce(J3D_loc_z, op=MPI.SUM)
+    comm.Barrier()
+    
+    if rank == 0:
+        print(pp.getJString(J3D_glob_x, J3D_glob_y, J3D_glob_z))
 
     # update
     wm1.x.array[:] = w.x.array[:]
