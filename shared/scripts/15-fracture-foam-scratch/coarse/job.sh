056418a3dc79671e96b2f865d3f914e0f25a95a3
Alex <alexander.schlueter86@gmx.de>
2024-07-04 08:59:08 +0200

    test scratch job

--- shared/scripts/15-fracture-foam-scratch/coarse/job.sh
+++ shared/scripts/15-fracture-foam-scratch/coarse/job.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH -J real_Al
+#SBATCH -J scratch_test
 #SBATCH -A project02338
 #SBATCH -t 1200  # "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
 #SBATCH --mem-per-cpu=6000
 #SBATCH -n 48
-#SBATCH -e /scratch/as12vapa/dolfinx_alex/shared/scripts/15-fracture-foam-scratch/coarse/%x.err.%j
-#SBATCH -o /scratch/as12vapa/dolfinx_alex/shared/scripts/15-fracture-foam-scratch/coarse/%x.out.%j
+#SBATCH -e /work/scratch/as12vapa/%x.err.%j
+#SBATCH -o /work/scratch/as12vapa/%x.out.%j
 #SBATCH --mail-type=End
 #SBATCH -C i01
 
-# # cd $HPC_SCRATCH
-cd /scratch/as12vapa/dolfinx_alex
+cd $HPC_SCRATCH
+# cd /scratch/as12vapa/dolfinx_alex
 
 # Parameters for simulation_script.py (passed as command-line arguments)
-srun -n 48 apptainer exec --bind ./shared:/home alex-dolfinx.sif python3 /home/scripts/15-fracture-foam-scratch/coarse/pfmfrac.py
+srun -n 48 apptainer exec --bind $HOME/dolfinx_alex/shared:/home $HOME/dolfinx_alex/alex-dolfinx.sif python3 /home/scripts/15-fracture-foam-scratch/coarse/pfmfrac.py
 
 EXITCODE=$?
 
