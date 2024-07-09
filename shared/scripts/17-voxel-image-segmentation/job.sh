11e9b8c0132ab79cd0cd0ae6fe9facc701890f5e
Alex <alexander.schlueter86@gmx.de>
2024-07-09 13:45:35 +0200

    writing files removed

--- shared/scripts/17-voxel-image-segmentation/job.sh
+++ shared/scripts/17-voxel-image-segmentation/job.sh
@@ -2,8 +2,8 @@
 #SBATCH -J cv_seg
 #SBATCH -A project02338
 #SBATCH -t 1200  # "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
-#SBATCH --mem-per-cpu=22000
-#SBATCH -n 1
+#SBATCH --mem-per-cpu=6000
+#SBATCH -n 48
 #SBATCH -e /work/scratch/as12vapa/%x.err.%j
 #SBATCH -o /work/scratch/as12vapa/%x.out.%j
 #SBATCH --mail-type=End
@@ -12,7 +12,7 @@
 cd $HPC_SCRATCH
 
 # Parameters for simulation_script.py (passed as command-line arguments)
-srun -n 1 apptainer exec --bind $HOME/dolfinx_alex/shared:/home $HOME/dolfinx_alex/alex-dolfinx.sif python3 /home/scripts/17-voxel-image-segmentation/chan-veese-segmentation.py
+srun -n 48 apptainer exec --bind $HOME/dolfinx_alex/shared:/home $HOME/dolfinx_alex/alex-dolfinx.sif python3 /home/scripts/17-voxel-image-segmentation/chan-veese-segmentation.py
 
 EXITCODE=$?
 
