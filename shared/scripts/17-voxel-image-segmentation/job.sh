0d11c40273590aaad26f46ba1fabb0d14fccafee
Alex <alexander.schlueter86@gmx.de>
2024-07-23 08:22:42 +0200

    staggered online script and image segmentation

--- shared/scripts/17-voxel-image-segmentation/job.sh
+++ shared/scripts/17-voxel-image-segmentation/job.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 1200  # "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
 #SBATCH --mem-per-cpu=6000
 #SBATCH -n 96
-#SBATCH -e /work/scratch/as12vapa/%x.err.%j
-#SBATCH -o /work/scratch/as12vapa/%x.out.%j
+#SBATCH -e /work/scratch/as12vapa/cv_seg/%x.err.%j
+#SBATCH -o /work/scratch/as12vapa/cv_seg/%x.out.%j
 #SBATCH --mail-type=End
 #SBATCH -C i01
 
@@ -20,7 +20,7 @@ mkdir -p "$working_directory"
 cd $HPC_SCRATCH
 
 # Parameters for simulation_script.py (passed as command-line arguments)
-srun -n 48 apptainer exec --bind $HOME/dolfinx_alex/shared:/home --bind "$working_directory:/work" $HOME/dolfinx_alex/alex-dolfinx.sif python3 /home/scripts/17-voxel-image-segmentation/chan-veese-segmentation.py
+srun -n 96 apptainer exec --bind $HOME/dolfinx_alex/shared:/home --bind "$working_directory:/work" $HOME/dolfinx_alex/alex-dolfinx.sif python3 /home/scripts/17-voxel-image-segmentation/chan-veese-segmentation.py
 
 EXITCODE=$?
 
