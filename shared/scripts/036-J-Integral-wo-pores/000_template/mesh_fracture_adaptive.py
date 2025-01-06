830378d12a73177ead1992b54de11f728aebf379
Alex <alexander.schlueter86@gmx.de>
2025-01-06 13:24:57 +0100

    reference computation wo pores

---
+++ shared/scripts/036-J-Integral-wo-pores/000_template/mesh_fracture_adaptive.py
@@ -0,0 +1,140 @@
+import pygmsh
+import meshio
+import numpy as np
+import alex.postprocessing
+import gmsh
+import os
+import argparse
+import math
+
+# Path setup
+script_path = os.path.dirname(__file__)
+script_name_without_extension = os.path.splitext(os.path.basename(__file__))[0]
+parameter_path = os.path.join(script_path, "parameters.txt")
+
+# Argument parsing
+parser = argparse.ArgumentParser(description="Run a simulation with specified parameters and organize output files.")
+try:
+    parser.add_argument("--nholes", type=int, required=True, help="Number of holes per row")
+    parser.add_argument("--dhole", type=float, required=True, help="Diameter of circular holes")
+    parser.add_argument("--wsteg", type=float, required=True, help="Width of steg")
+    parser.add_argument("--e0", type=float, required=True, help="Size of elements")
+    parser.add_argument("--e1", type=float, required=True, help="Coarse element size")
+    args = parser.parse_args()
+    Nholes = args.nholes
+    dhole = args.dhole
+    wsteg = args.wsteg
+    e0 = args.e0  # Fine mesh size
+    e1 = args.e1  # Coarse mesh size
+except:
+    print("Could not parse arguments")
+    Nholes = 4
+    dhole = 1.0
+    wsteg = 4.0
+    e0 = 0.02  # Fine mesh size
+    e1 = 0.8  # Coarse mesh size
+
+# Derived geometry parameters
+w_cell = dhole + wsteg
+h_cell = w_cell
+l0 = (Nholes + 2) * w_cell
+h0 = 20.0  # Adjust height to accommodate 3 rows
+n_rows = 3
+filename = os.path.join(script_path, script_name_without_extension)
+mesh_info = True
+
+# Create geometry with pygmsh
+geom = pygmsh.occ.Geometry()
+model = geom.__enter__()
+
+# Outer matrix definition
+p0 = model.add_point([0, -h0 / 2])
+p1 = model.add_point([l0, -h0 / 2])
+p2 = model.add_point([l0, +h0 / 2])
+p3 = model.add_point([0, h0 / 2])
+line0 = model.add_line(p0, p1)  # Bottom
+line1 = model.add_line(p1, p2)  # Right
+line2 = model.add_line(p2, p3)  # Top
+line3 = model.add_line(p3, p0)  # Left
+outer = model.add_curve_loop([line0, line1, line2, line3])
+eff_matr = model.add_plane_surface(outer)
+
+# Generate unit cells without holes
+cell_matrix_list = []
+for i in range(n_rows):  # Iterate over rows
+    for n in range(Nholes):  # Iterate over columns
+        x_center = w_cell + w_cell / 2.0 + n * w_cell
+        y_center = -(n_rows // 2) * h_cell + i * h_cell
+
+        left_bottom_rectangle = [x_center - w_cell / 2.0, y_center - h_cell / 2, 0]
+        cell_matrix_tmp = model.add_rectangle(left_bottom_rectangle, w_cell, h_cell, 0)
+        cell_matrix_list.append(cell_matrix_tmp)
+
+# Define effective matrix and cell matrix separately
+eff_matr_group = model.boolean_fragments([eff_matr], cell_matrix_list, delete_first=False, delete_other=False)
+
+# Crack definition
+p8 = model.add_point([0.0, 0.0])
+p9 = model.add_point([l0, 0.0])
+crack = model.add_line(p8, p9)
+
+# Mesh refinement fields
+gmsh.model.mesh.field.add("Distance", 1)
+refinement_points = [model.add_point([w_cell + w_cell / 2.0 + n * w_cell, -(n_rows // 2) * h_cell + i * h_cell, 0.0], e0)._id for i in range(n_rows) for n in range(Nholes)]
+
+crack_points = [
+    model.add_point([i * l0 / 50, 0.0, 0.0], e0)._id
+    for i in range(51)
+]
+refinement_points += crack_points
+gmsh.model.mesh.field.setNumbers(1, "NodesList", refinement_points)
+
+gmsh.model.mesh.field.add("Distance", 2)
+gmsh.model.mesh.field.setNumbers(2, "EdgesList", [crack._id])
+
+gmsh.model.mesh.field.add("Min", 3)
+gmsh.model.mesh.field.setNumbers(3, "FieldsList", [1, 2])
+
+gmsh.model.mesh.field.add("Threshold", 4)
+gmsh.model.mesh.field.setNumber(4, "InField", 3)
+gmsh.model.mesh.field.setNumber(4, "SizeMin", e0)
+gmsh.model.mesh.field.setNumber(4, "SizeMax", e1)
+gmsh.model.mesh.field.setNumber(4, "DistMin", dhole / 2)
+gmsh.model.mesh.field.setNumber(4, "DistMax", w_cell)
+
+gmsh.model.mesh.field.setAsBackgroundMesh(4)
+
+# Assign physical groups
+model.synchronize()
+model.add_physical(eff_matr, 'eff_matrix')
+model.add_physical(cell_matrix_list, "cell_matrix")
+model.add_physical(crack, 'crack')
+
+# Generate and save mesh
+model.generate_mesh(dim=2, verbose=True)
+gmsh.write(filename + ".msh")
+gmsh.clear()
+model.__exit__()
+
+# Post-process mesh
+mesh = meshio.read(filename + ".msh")
+nodes = mesh.points[:, :2]
+elems = mesh.get_cells_type("triangle")
+elem_data = mesh.cell_data_dict["gmsh:physical"]["triangle"] - 1
+
+def filter_points_and_update_cells(cell_array, point_array):
+    referenced_ids = {point_id for triangle in cell_array for point_id in triangle}
+    filtered_points = [point_array[i] for i in referenced_ids]
+    mapping = {old: new for new, old in enumerate(referenced_ids)}
+    updated_cells = [[mapping[pt] for pt in triangle] for triangle in cell_array]
+    return np.array(filtered_points), np.array(updated_cells)
+
+nodes, elems = filter_points_and_update_cells(elems, nodes)
+
+# Save final mesh
+cell_mesh = meshio.Mesh(points=nodes, cells={"triangle": elems}, cell_data={"name_to_read": [elem_data]})
+meshio.write(filename + ".xdmf", cell_mesh)
+
+# Save parameters
+params = {"nholes": Nholes, "dhole": dhole, "wsteg": wsteg, "e0": e0, "e1": e1}
+alex.postprocessing.append_to_file(parameter_path, params)
---
+++ shared/scripts/036-J-Integral-wo-pores/00_jobs/job_template_adaptive.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH -J {JOB_NAME}
+#SBATCH -A p0023647
+#SBATCH -t {TIME}  # "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds"
+#SBATCH --mem-per-cpu={MEMORY_VALUE}
+#SBATCH -n {PROCESSOR_NUMBER}
+#SBATCH -e /work/scratch/as12vapa/036-J-Integral-wo-pores/{FOLDER_NAME}/%x.err.%j
+#SBATCH -o /work/scratch/as12vapa/036-J-Integral-wo-pores/{FOLDER_NAME}/%x.out.%j
+#SBATCH --mail-type=End
+#SBATCH -C i01
+
+# Set the working directory name
+working_folder_name="{FOLDER_NAME}"  # Change this to your desired folder name
+# Create the working directory under $HPC_SCRATCH
+working_directory="$HPC_SCRATCH/036-J-Integral-wo-pores/$working_folder_name"
+
+# Default values for parameters
+NHOLES={NHOLES} # needs to be int
+WSTEG={WSTEG}
+DHOLE={DHOLE}
+E0={E0}
+E1={E1}
+HOLE_ANGLE={HOLE_ANGLE} # Default angle for the diamond-shaped holes
+MESH_FILE="{MESH_FILE}"
+LAM_MICRO_PARAM={LAM_MICRO_PARAM}
+MUE_MICRO_PARAM={MUE_MICRO_PARAM}
+GC_MICRO_PARAM={GC_MICRO_PARAM}
+
+# Calculate EPS_PARAM as 6 times E0 using awk if not provided by user
+EPS_PARAM={EPS_PARAM}
+ELEMENT_ORDER={ELEMENT_ORDER}
+
+LCRACK=$(awk "BEGIN {print $WSTEG + $DHOLE}")
+
+# Navigate to $HPC_SCRATCH
+cd $HPC_SCRATCH
+
+# Run the mesh generation and other scripts with --hole_angle parameter
+srun -n 1 apptainer exec --bind $HOME/dolfinx_alex/shared:/home,$working_directory:/work $HOME/dolfinx_alex/alex-dolfinx.sif python3 $working_directory/mesh_effective_stiffness.py --dhole "$DHOLE" --wsteg "$WSTEG" --e0 "$E0" --hole_angle "$HOLE_ANGLE"
+
+srun -n 1 apptainer exec --bind $HOME/dolfinx_alex/shared:/home,$working_directory:/work $HOME/dolfinx_alex/alex-dolfinx.sif python3 $working_directory/run_effective_stiffness.py --lam_micro_param "$LAM_MICRO_PARAM" --mue_micro_param "$MUE_MICRO_PARAM"
+
+srun -n 1 apptainer exec --bind $HOME/dolfinx_alex/shared:/home,$working_directory:/work $HOME/dolfinx_alex/alex-dolfinx.sif python3 $working_directory/mesh_fracture_adaptive.py --nholes "$NHOLES" --dhole "$DHOLE" --wsteg "$WSTEG" --e0 "$E0" --e1 "$E1" --hole_angle "$HOLE_ANGLE"
+
+srun -n 1 apptainer exec --bind $HOME/dolfinx_alex/shared:/home,$working_directory:/work $HOME/dolfinx_alex/alex-dolfinx.sif python3 $working_directory/get_mesh_info.py --mesh_file "$MESH_FILE"
+
+# Parameters for simulation_script.py (passed as command-line arguments)
+srun -n {PROCESSOR_NUMBER} apptainer exec --bind $HOME/dolfinx_alex/shared:/home,$working_directory:/work $HOME/dolfinx_alex/alex-dolfinx.sif python3 $working_directory/run_simulation.py --mesh_file "$MESH_FILE" --in_crack_length "$LCRACK" --lam_micro_param "$LAM_MICRO_PARAM" --mue_micro_param "$MUE_MICRO_PARAM" --gc_micro_param "$GC_MICRO_PARAM" --eps_param "$EPS_PARAM" --element_order "$ELEMENT_ORDER"
+
+EXITCODE=$?
+
+# Exit the job script with the status of the scientific program
+exit $EXITCODE
+
+
+
+
+
+
+
